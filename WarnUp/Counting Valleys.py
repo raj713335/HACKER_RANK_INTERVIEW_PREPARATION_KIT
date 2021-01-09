6c33e7571f77d70805e4d7b31056952ea413815b
ARNAB DAS <raj713335@gmail.com>
2021-01-09 23:45:47 +0530

    Add files via upload

---
+++ WarnUp/Counting Valleys.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingValleys' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER steps
+#  2. STRING path
+#
+
+def countingValleys(steps, path):
+    
+    elevation=0
+    vally_count=0
+    
+    for steps in path:
+        if steps=='U':
+            elevation+=1
+            if elevation==0:
+                vally_count+=1
+        if steps=='D':
+            elevation-=1
+            
+    return (vally_count)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    steps = int(input().strip())
+
+    path = input()
+
+    result = countingValleys(steps, path)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
