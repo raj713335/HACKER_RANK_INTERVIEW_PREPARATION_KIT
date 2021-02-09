43fed7e78cb1c662bd83af3f1c5ff9f488a8e0e0
ARNAB DAS <raj713335@gmail.com>
2021-02-09 18:12:17 +0530

    Add files via upload

---
+++ Arrays/2D Arrays - DS.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the hourglassSum function below.
+def hourglassSum(arr):
+    
+    max_hour_glass=-math.inf
+    
+    for i in range(0,4):
+        for j in range(0,4):
+            temp = arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2]
+            
+            if temp>max_hour_glass:
+                max_hour_glass=temp
+                
+    
+    return max_hour_glass
+        
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    arr = []
+
+    for _ in range(6):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = hourglassSum(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
